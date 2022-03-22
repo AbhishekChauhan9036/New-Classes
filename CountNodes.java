2d588e03d37c212c9d3e634e25fc206ca9472a48
Abhi <abhishekchauhan9036@gmail.com>
2022-03-22 21:40:40 +0530

    commit message

---
+++ CountNodes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class CountNodes{
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data){
+			this.data=data;
+			this.left=null;
+			this.right=null;
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
+}
